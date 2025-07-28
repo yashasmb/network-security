d4c3a7a74b9a8b3a1cff1a8cb13c447455cc581c
Yashas M B <yashasmb2003@gmail.com>
2025-07-29 00:29:13 +0530

    made changes to config entity for data injection

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -24,14 +24,15 @@ class TrainingPipelineConfig:
         
         self.pipeline_name = training_pipeline.PIPELINE_NAME
         self.artifact_dir = training_pipeline.ARTIFACT_DIR
+        self.timestamp = timestamp 
         self.artifacts_name = os.path.join(self.artifact_dir, timestamp)
-        self.timestamp = timestamp  
+         
 
 
 class DataIngestionConfig:
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
         self.data_ingestion_dir:str=os.path.join(
-            training_pipeline_config.artifact_dir,training_pipeline.DATA_INGESTION_DIR_NAME
+            training_pipeline_config.artifacts_name
         )
         self.feature_store_file_path: str = os.path.join(
                 self.data_ingestion_dir, training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR, training_pipeline.FILE_NAME
