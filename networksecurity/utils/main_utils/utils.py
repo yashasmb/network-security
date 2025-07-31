b444b2f7db03fbcc8067faab2a2f87a138702188
Yashas M B <yashasmb2003@gmail.com>
2025-07-31 23:33:48 +0530

    changes to data_validation files

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -13,3 +13,5 @@ def read_yaml_file(file_path: str) -> dict:
             return yaml.safe_load(yaml_file)
     except Exception as e:
         raise NetworksecurityException(e, sys) from e
+    
+def write_yaml_file(file_path :)
